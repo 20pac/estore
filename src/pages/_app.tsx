dad8d73bb7b6b2b4aaae01779bc6622046579686
Ananya Singh <ananyahimself@gmail.com>
2021-12-20 19:35:06 +0530

    setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,15 @@
 import "@src/styles.css";
 
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-import { setUpAnalytics } from "@src/utils/firebase";
-
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    setUpAnalytics();
-  }, []);
-
   return (
     <>
       <Head>
-        <title>Worldschool</title>
+        <title>NextJS + Tailwind Boilerplate</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import { mediaStyle } from "../utils/media";
-
 export default class MyDocument extends Document {
   render() {
     return (
@@ -15,9 +13,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
-          <style type="text/css" dangerouslySetInnerHTML={{ __html: mediaStyle }} />
         </Head>
-        <body style={{ display: "flex", justifyContent: "center" }}>
+        <body>
           <Main />
           <NextScript />
         </body>
